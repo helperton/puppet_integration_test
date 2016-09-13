f6794936061729e66ee905d5cb5a2b6662580599
Joshua McClintock <joshua.mcclintock@gmail.com>
2016-09-13 14:13:30 -0700

    huh?

--- test_integration.rb
+++ test_integration.rb
@@ -35,7 +35,7 @@ def ssh_command(cmd, options = { :p_stdout => false, :p_stderr => false })
           exit_code = data.read_long
         end
 
-        ch.on_close { print "\n\nCommand:\n\n#{cmd.colorize(:light_blue)}\n\nExited with code: #{exit_code}\n\n" }
+        ch.on_close { print "\n\nCommand:\n\n#{cmd.colorize(:blue)}\n\nExited with code: #{exit_code}\n\n" }
       end
     end
     channel.wait
