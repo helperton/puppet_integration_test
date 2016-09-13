4f9be0434949058ba58bd5154e4423e2d55a1513
Joshua McClintock <joshua.mcclintock@gmail.com>
2016-09-13 11:11:40 -0700

    expiriment

--- test_integration.rb
+++ test_integration.rb
@@ -3,7 +3,7 @@
 $host = ARGV[0]
 
 def ssh_command(cmd, host = $host)
-  %x(ssh -q -T -o ConnectTimeout=10 -o StrictHostKeyChecking=no root@#{host} #{cmd})
+  %X(ssh -q -T -o ConnectTimeout=10 -o StrictHostKeyChecking=no root@#{host} #{cmd})
 end
 
 def which_os
