4e9c90cf638673225bd446280b71c07a499a910b
Joshua McClintock <joshua.mcclintock@gmail.com>
2016-09-12 23:22:08 -0700

    Adding ssh command

--- test_integration.rb
+++ test_integration.rb
@@ -1,1 +1,9 @@
 #!/usr/bin/env ruby
+
+$host = $ARGV[0]
+
+def ssh_command(cmd, host = $host)
+    "ssh -q -T -o ConnectTimeout=10 -o StrictHostKeyChecking=no #{host} #{cmd}"
+end
+
+ssh_command("ls")
