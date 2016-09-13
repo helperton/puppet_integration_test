0a5c348a516835632280152a4a4292aa02662ccc
Joshua McClintock <joshua.mcclintock@gmail.com>
2016-09-13 10:43:10 -0700

    adding in functions to handle reboot, rsync, newlines, etc

--- test_integration.rb
+++ test_integration.rb
@@ -3,7 +3,73 @@
 $host = ARGV[0]
 
 def ssh_command(cmd, host = $host)
-  %x(ssh -q -T -o ConnectTimeout=10 -o StrictHostKeyChecking=no root@#{host} #{cmd})
+  puts %x(ssh -q -T -o ConnectTimeout=10 -o StrictHostKeyChecking=no root@#{host} #{cmd})
 end
 
-puts ssh_command("ls")
+def which_os
+  ssh_command("uname -s").chomp.downcase
+end
+
+def rsync_revert
+  rsync_cmd = String.new
+
+  case which_os
+  when "linux"
+    rsync_cmd = "rsync -av --progress --delete --exclude /var/recover --exclude /dev --exclude /proc --exclude /sys --exclude /selinux --exclude '/var/run/*.pid' --exclude '/var/run/*/*.pid' --exclude /nfs --exclude /var/lib/nfs /var/recover/ /"
+  when "aix"
+    rsync_cmd = "rsync -av --progress --delete --exclude /var/recover --exclude /dev --exclude /proc --exclude /nfs /var/recover/ /"
+  when "sunos"
+   rsync_cmd = "rsync -av --progress --delete --exclude /var/recover --exclude /dev --exclude /devices --exclude /proc --exclude /system --exclude /nfs --exclude /etc/svc/volatile --exclude /etc/mnttab --exclude /etc/dfs/sharetab --exclude /var/run --exclude '/etc/sysevent/*door*' --exclude '/etc/sysevent/*channel*' --exclude /rpool /var/recover/ /"
+  end
+
+  print "#{nls}Reverting host to fresh state ..."
+  ssh_command(rsync_cmd)
+  print " done.#{nls}"
+end
+
+def reboot_command(host = $host)
+  case which_os
+  when "linux"
+    "reboot"
+  when "aix"
+    "shutdown -Fr"
+  when "sunos"
+    "init 6"
+  end
+end
+
+def nls(num = 3)
+  newlines = Array.new
+  1.upto(num) do |n|
+    newlines.push("\n")
+  end
+  newlines.join("")
+end
+
+def reboot_host
+  reboot_and_wait_for_host
+end
+
+def reboot_and_waitforhost(host = $host)
+  stop_agent(host)
+  puts "#{nls}Rebooting host and waiting ...#{nls}"
+  ret = %x(#{ssh_command} "nohup #{reboot_command} &")
+  puts ret
+  sleep 10
+  status = 1
+  while status > 0
+    check_if_host_rebooting(host)
+    puts "#{nls}Verifying host rebooted ...#{nls}"
+    %x(#{ssh_command} ls >/dev/null)
+    status = $?.exitstatus
+    sleep 10
+  end
+  ret = %x(#{ssh_command} ls)
+  puts "#{nls}Host is back up!#{nls}"
+  ret
+end
+
+
+# Begin main script
+rsync_revert
+reboot_host
