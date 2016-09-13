385703e98d7f583fbe245fd4a85e72af862777ec
Joshua McClintock <joshua.mcclintock@gmail.com>
2016-09-13 14:28:40 -0700

    make the same?

--- test_integration.rb
+++ test_integration.rb
@@ -120,7 +120,7 @@ def is_host_rebooting?
     sleep 10
     begin
       status = Timeout::timeout(20) {
-        ret = ssh_command("who -r", :ssh_timeout => 0)[:stdout].first.split(/\s+/)[3].to_i
+        ret = ssh_command("who -r", :ssh_timeout => 20)[:stdout].first.split(/\s+/)[3].to_i
       }
     rescue Timeout::Error => e
       puts "Exception #{e} occured, continuing..."
